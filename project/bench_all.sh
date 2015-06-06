f9679e93cbd0fef167ad289c611f1c2c407f334c
Sam Toyer <sam@qxcv.net>
2015-06-07 00:34:38 +1000

    Work on benchmark script

--- project/bench_all.sh
+++ project/bench_all.sh
@@ -5,6 +5,7 @@
 COMMON_OPTIONS="--particles 2000"
 RUN="python2 run.py $COMMON_OPTIONS"
 DEST_DIR=results/
+MANGLE="python2 mangle_data.py"
 
 mkdir -p "$DEST_DIR"
 
@@ -17,16 +18,38 @@ child_block() {
     done
 }
 
-for id in {00..10}; do
-    trajectory=data/kitti/map_trajectories/${id}.txt.bz2
-    map=data/kitti/${id}.osm.bz2
-    echo "Spawning processes for trajectory $id"
-    child_block
-    $RUN --enablemapfilter --out "$DEST_DIR/${id}-map.csv" "$trajectory" "$map" &
-    child_block
-    $RUN --enableplainfilter --out "$DEST_DIR/${id}-plain.csv" "$trajectory" "$map" &
-    echo "Spawning for $id done"
-    sleep 2
-done
+scrape() {
+    # Scrape generated CSVs to produce something that can be thrown in the
+    # final report
+    for id in {00..10}; do
+        prefix="$DEST_DIR/${id}"
+        $MANGLE --mapfilter "$prefix-map.csv"
+        $MANGLE --plainfilter "$prefix-plain.csv"
+    done
+}
 
-wait
+case $1 in
+    scrape)
+        scrape
+        ;;
+    *)
+        echo "Writing CSVs"
+        for id in {00..10}; do
+            trajectory=data/kitti/map_trajectories/${id}.txt.bz2
+            map=data/kitti/${id}.osm.bz2
+            echo "Spawning processes for trajectory $id"
+            child_block
+            $RUN --enablemapfilter --out "$DEST_DIR/${id}-map.csv" \
+                "$trajectory" "$map" &
+            child_block
+            $RUN --enableplainfilter --out "$DEST_DIR/${id}-plain.csv" \
+                "$trajectory" "$map" &
+            echo "Spawning for $id done"
+            sleep 2
+        done
+
+        wait
+        scrape
+        ;;
+esac
+
