737ff317dcc64d2b5c5ca0ca4e15506521abbfa2
Sam Toyer <sam@qxcv.net>
2015-06-08 11:56:38 +1000

    Add box plot to report.

--- project/bench_all.sh
+++ project/bench_all.sh
@@ -27,7 +27,8 @@ scrape() {
         args="$args?:delim:?$prefix-map.csv?:delim:?$prefix-plain.csv"
     done
 
-python3 - "$args" <<EOF
+    script="`mktemp`"
+    cat > "$script" <<PYTHON
 from itertools import *
 from sys import argv
 from os.path import basename
@@ -36,6 +37,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
 
 # Load data with Pandas
 paths = argv[1].split('?:delim:?')[1:]
@@ -47,29 +49,61 @@ map_hpe = [table['map_hpe'].as_matrix() for table in with_map]
 plain_hpe = [table['plain_hpe'].as_matrix() for table in without_map]
 all_hpes = list(chain.from_iterable(zip(map_hpe, plain_hpe)))
 
+colours = (
+    (0.161, 0.31, 0.427),
+    (0.667, 0.475, 0.224)
+)
+
 # Make Matplotlib use Computer Modern for our PGF diagrams
 matplotlib.rcParams.update({
     'font.family': 'serif',
     'pgf.rcfonts': False,
-    'pgf.texsystem': 'pdflatex'
+    'pgf.texsystem': 'pdflatex',
+    'xtick.labelsize': 'x-small',
+    'ytick.labelsize': 'x-small',
+    'axes.labelsize': 'small'
 })
 
 # Now make a box chart showing horizontal positioning error
-plt.boxplot(all_hpes)
+bp = plt.boxplot(all_hpes)
+plt.setp(bp['boxes'], color='black')
+plt.setp(bp['whiskers'], color='black')
+plt.setp(bp['fliers'], color='black')
+rects = []
+for idx, box in enumerate(bp['boxes']):
+    box.set_fillstyle('none')
+    xdata = box.get_xdata()
+    xstart = min(xdata)
+    width = max(xdata) - xstart
+    ydata = box.get_ydata()
+    ystart = min(ydata)
+    height = max(ydata) - ystart
+    colour = colours[idx % len(colours)]
+    r = Rectangle(
+        (xstart, ystart), width, height, zorder=-1, fc=colour, ec='none'
+    )
+    plt.gca().add_patch(r)
+    rects.append(r)
+
 plt.xticks(np.arange(len(all_hpes))+1, map_names)
+plt.legend(
+    (rects[0], rects[1]), ('With map', 'Without map'), loc='best',
+    fontsize='small'
+)
 plt.xlabel('Data set')
 plt.ylabel('HPE (m)')
 plt.title('Particle filter horizontal positioning error')
-plt.ylim(ymax=100, ymin=0)
+plt.ylim(ymax=40, ymin=0)
 
 # Remove redundant whitespace and fit plot to a single page
-plt.gcf().set_size_inches((7, 4))
+# TODO: Make the plot wider
+plt.gcf().set_size_inches((5, 4))
 plt.tight_layout()
-plt.savefig('results/boxplot.png')
-plt.savefig('results/boxplot.pgf')
+plt.savefig('report/images/boxplot.pgf')
 print("Success!")
-EOF
-
+PYTHON
+python3 "$script" "$args" || echo "Python failed :("
+rm "$script"
 }
 
 case $1 in
