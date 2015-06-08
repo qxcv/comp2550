769912459786e1d2bec3ed61a8b08572bb082921
Sam Toyer <sam@qxcv.net>
2015-06-08 11:01:31 +1000

    Work on plotting final results

--- project/bench_all.sh
+++ project/bench_all.sh
@@ -21,11 +21,55 @@ child_block() {
 scrape() {
     # Scrape generated CSVs to produce something that can be thrown in the
     # final report
+    args=""
     for id in {00..10}; do
         prefix="$DEST_DIR/${id}"
-        $MANGLE --mapfilter "$prefix-map.csv"
-        $MANGLE --plainfilter "$prefix-plain.csv"
+        args="$args?:delim:?$prefix-map.csv?:delim:?$prefix-plain.csv"
     done
+
+python3 - "$args" <<EOF
+from itertools import *
+from sys import argv
+from os.path import basename
+
+import pandas as pd
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
+# Load data with Pandas
+paths = argv[1].split('?:delim:?')[1:]
+map_names = [basename(p).split('-')[0] for p in paths]
+loaded = list(map(pd.read_csv, paths))
+with_map = loaded[::2]
+without_map = loaded[1::2]
+map_hpe = [table['map_hpe'].as_matrix() for table in with_map]
+plain_hpe = [table['plain_hpe'].as_matrix() for table in without_map]
+all_hpes = list(chain.from_iterable(zip(map_hpe, plain_hpe)))
+
+# Make Matplotlib use Computer Modern for our PGF diagrams
+matplotlib.rcParams.update({
+    'font.family': 'serif',
+    'pgf.rcfonts': False,
+    'pgf.texsystem': 'pdflatex'
+})
+
+# Now make a box chart showing horizontal positioning error
+plt.boxplot(all_hpes)
+plt.xticks(np.arange(len(all_hpes))+1, map_names)
+plt.xlabel('Data set')
+plt.ylabel('HPE (m)')
+plt.title('Particle filter horizontal positioning error')
+plt.ylim(ymax=100, ymin=0)
+
+# Remove redundant whitespace and fit plot to a single page
+plt.gcf().set_size_inches((7, 4))
+plt.tight_layout()
+plt.savefig('results/boxplot.png')
+plt.savefig('results/boxplot.pgf')
+print("Success!")
+EOF
+
 }
 
 case $1 in
